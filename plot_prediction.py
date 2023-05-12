8c116e342153be2476a09d4ba7058e80ad29bd2f
milan3000 <milanw12@gmail.com>
2023-05-12 17:39:47 +0200

    switched to plotly

--- plot_prediction.py
+++ plot_prediction.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-import ipywidgets as widgets
-import matplotlib.pyplot as plt
+import dash
+from dash import dcc
+from dash import html
+import plotly.graph_objs as go
 
 prediction_df = pd.read_csv('current_prediction.csv', parse_dates=['time'])
 
@@ -13,42 +15,74 @@ y_solar = np.asarray(prediction_df['solar'])
 y_demand = np.asarray(prediction_df['demand'])
 y_residual = y_demand - (y_biomass + y_hydropower + y_wind + y_solar)
 
-color_biomass = (0.3176, 0.7019, 0.3176)
-color_hydropower = (0.6510, 0.8862, 1)
-color_wind = (0.3529,0.4039,1)
-color_solar = (1, 0.9529, 0.2509)
-color_demand = (1, 0.2509, 0)
-color_residual = (0.4588, 0.5019, 0.4588, 0.5)
+color_biomass = (80.988, 178.985, 80.988)
+color_hydropower = (166.005, 225.981, 255)
+color_wind = (89.9895, 102.995, 255)
+color_solar = (255, 242.99, 63.9795)
+color_demand = (255, 64, 0)
+color_residual = (116.994, 127.985, 116.994, 0.5)
 
-def plot_prediction(detail_index):
-    fig, ax = plt.subplots(figsize=(8,5))     
-    ax.stackplot(time_axis, y_biomass, y_hydropower, y_wind, y_solar, y_residual, colors = [color_biomass, color_hydropower, color_wind, color_solar, color_residual])
-    ax.plot(time_axis, y_demand, color = color_demand)
-    plt.xticks(rotation=45)
-    ax.grid(alpha=0.3)
-    ax.set_ylabel('[MWh]')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1], f'Date: {time_axis[detail_index].date()}')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-5000, f'Time: {time_axis[detail_index].time()}')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-10000, f'■', color=color_demand)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-10000, f'Demand: {round(y_demand[detail_index])} MWh')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-15000, f'■', color=color_residual)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-15000, f'Residual Load: {round(y_residual[detail_index])} MWh')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-20000, f'■', color=color_solar)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-20000, f'Solar: {round(y_solar[detail_index])} MWh')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-25000, f'■', color=color_wind)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-25000, f'Wind: {round(y_wind[detail_index])} MWh')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-30000, f'■', color=color_hydropower)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-30000, f'Hydropower: {round(y_hydropower[detail_index])} MWh')
-    ax.text(ax.get_xlim()[1]+0.1, ax.get_ylim()[1]-35000, f'■', color=color_biomass)
-    ax.text(ax.get_xlim()[1]+0.25, ax.get_ylim()[1]-35000, f'Biomass: {round(y_biomass[detail_index])} MWh')
-    plt.axvline(x = time_axis[detail_index], color = 'black', ymin=0, ymax=y_demand[detail_index]/ax.get_ylim()[1], alpha=0.5)
-    plt.plot(time_axis[detail_index], y_demand[detail_index], 'o', color=color_demand, alpha=0.6)
-    plt.plot(time_axis[detail_index], y_solar[detail_index]+y_wind[detail_index]+y_hydropower[detail_index]+y_biomass[detail_index], 'o', color=color_solar, alpha=0.6)
-    plt.plot(time_axis[detail_index], y_wind[detail_index]+y_hydropower[detail_index]+y_biomass[detail_index], 'o', color=color_wind, alpha=0.6)
-    plt.plot(time_axis[detail_index], y_hydropower[detail_index]+y_biomass[detail_index], 'o', color=color_hydropower, alpha=0.6)
-    plt.plot(time_axis[detail_index], y_biomass[detail_index], 'o', color=color_biomass, alpha=0.6)
-    plt.show()
+app = dash.Dash(__name__)
+server = app.server
 
+# create the figure object
+fig = go.Figure()
+# add traces to the figure
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_biomass,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_biomass)}'),
+    stackgroup='one', # define stack group
+    name='Biomass'
+))
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_hydropower,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_hydropower)}'),
+    stackgroup='one',
+    name='Hydropower'
+))
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_wind,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_wind)}'),
+    stackgroup='one',
+    name='Wind'
+))
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_solar,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_solar)}'),
+    stackgroup='one',
+    name='Solar'
+))
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_residual,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_residual)}'),
+    stackgroup='one',
+    name='Residual Load'
+))
+fig.add_trace(go.Scatter(
+    x=time_axis, y=y_demand,
+    hoverinfo='x+y',
+    mode='lines',
+    line=dict(width=0.5, color=f'rgb{(color_demand)}'),
+    name='Demand'
+))
 
-interactive_plot = widgets.interact(plot_prediction, detail_index=widgets.IntSlider(0,0,167,1, layout=widgets.Layout(width='50%')), continuous_update=False)
-interactive_plot
+fig.update_layout(xaxis_title="Time", yaxis_title="[MWh]")
+
+# create the layout of the app
+app.layout = html.Div(children=[
+    html.H1(children='Germany Electricity Generation Forecast'),
+    dcc.Graph(id='example-graph', figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True, use_reloader=False)
