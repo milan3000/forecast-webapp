f2013373620ff5c623221c525b39010e12613a45
milan3000 <milanw12@gmail.com>
2023-05-14 22:14:57 +0200

    added icon, changed layout

--- plot_prediction.py
+++ plot_prediction.py
@@ -15,14 +15,15 @@ y_solar = np.asarray(prediction_df['solar'])
 y_demand = np.asarray(prediction_df['demand'])
 y_residual = y_demand - (y_biomass + y_hydropower + y_wind + y_solar)
 
-color_biomass = (80.988, 178.985, 80.988)
-color_hydropower = (166.005, 225.981, 255)
-color_wind = (89.9895, 102.995, 255)
-color_solar = (255, 242.99, 63.9795)
-color_demand = (255, 64, 0)
-color_residual = (116.994, 127.985, 116.994, 0.5)
+color_biomass = (80.988, 178.985, 80.988, 0.8)
+color_hydropower = (166.005, 225.981, 255, 0.8)
+color_wind = (89.9895, 102.995, 255, 0.8)
+color_solar = (255, 242.99, 63.9795, 0.8)
+color_demand = (255, 64, 0, 0.8)
+color_residual = (116.994, 127.985, 116.994, 0.8)
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, assets_folder='assets')
+app.title = 'RE Forecast'
 server = app.server
 
 # create the figure object
@@ -34,7 +35,8 @@ fig.add_trace(go.Scatter(
     mode='lines',
     line=dict(width=0.5, color=f'rgb{(color_biomass)}'),
     stackgroup='one', # define stack group
-    name='Biomass'
+    name='Biomass',
+    fillcolor=f'rgba{(color_biomass)}'
 ))
 fig.add_trace(go.Scatter(
     x=time_axis, y=y_hydropower,
@@ -42,7 +44,8 @@ fig.add_trace(go.Scatter(
     mode='lines',
     line=dict(width=0.5, color=f'rgb{(color_hydropower)}'),
     stackgroup='one',
-    name='Hydropower'
+    name='Hydropower',
+    fillcolor=f'rgba{(color_hydropower)}'
 ))
 fig.add_trace(go.Scatter(
     x=time_axis, y=y_wind,
@@ -50,7 +53,8 @@ fig.add_trace(go.Scatter(
     mode='lines',
     line=dict(width=0.5, color=f'rgb{(color_wind)}'),
     stackgroup='one',
-    name='Wind'
+    name='Wind',
+    fillcolor=f'rgba{(color_wind)}'
 ))
 fig.add_trace(go.Scatter(
     x=time_axis, y=y_solar,
@@ -58,7 +62,8 @@ fig.add_trace(go.Scatter(
     mode='lines',
     line=dict(width=0.5, color=f'rgb{(color_solar)}'),
     stackgroup='one',
-    name='Solar'
+    name='Solar',
+    fillcolor=f'rgba{(color_solar)}'
 ))
 fig.add_trace(go.Scatter(
     x=time_axis, y=y_residual,
@@ -66,22 +71,31 @@ fig.add_trace(go.Scatter(
     mode='lines',
     line=dict(width=0.5, color=f'rgb{(color_residual)}'),
     stackgroup='one',
-    name='Residual Load'
+    name='Residual Load',
+    fillcolor=f'rgba{(color_residual)}'
 ))
 fig.add_trace(go.Scatter(
     x=time_axis, y=y_demand,
     hoverinfo='x+y',
     mode='lines',
-    line=dict(width=0.5, color=f'rgb{(color_demand)}'),
+    line=dict(width=1.25, color=f'rgb{(color_demand)}'),
     name='Demand'
 ))
 
-fig.update_layout(xaxis_title="Time", yaxis_title="[MWh]")
+fig.update_xaxes(showgrid=True, gridwidth=0.2, gridcolor='rgba(0, 0, 0, 0.3)')
+fig.update_yaxes(showgrid=True, gridwidth=0.2, gridcolor='rgba(0, 0, 0, 0.3)')
+fig.update_traces(hovertemplate='%{x}<br>%{y:.2f} MWh')
+
+fig.update_layout(xaxis_title="Time", 
+                  yaxis_title="[MWh]", 
+                  title="Forecast Electricity generation and consumption in Germany",
+                  plot_bgcolor='rgba(255, 255, 255, 1)', # set background color with lower alpha value
+                  paper_bgcolor='rgba(255, 255, 255, 1)'            
+)
 
 # create the layout of the app
 app.layout = html.Div(children=[
-    html.H1(children='Germany Electricity Generation Forecast'),
-    dcc.Graph(id='example-graph', figure=fig)
+    dcc.Graph(id='forecast-graph', figure=fig),
 ])
 
 if __name__ == '__main__':
