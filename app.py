11c1999498e56b567c666e290a8209aad0729424
milan3000 <milanw12@gmail.com>
2023-06-05 18:39:37 +0200

    initial (unstable) implement of multiple pages

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from dash import html
 from plot_prediction import plot_prediction
 from plot_map import plot_map
 
-app = dash.Dash(__name__, assets_folder='assets')
+app = dash.Dash(__name__, assets_folder='assets', use_pages=True)
 app.title = 'Renewable Energy Forecast Germany'
 server = app.server
 
-fig1 = plot_prediction()
-fig2 = plot_map()
-
 # create the layout of the app
 app.layout = html.Div(children=[
-    html.Div([dcc.Graph(id='graph1',figure=fig1)]),
-    html.Div([dcc.Graph(id='graph2',figure=fig2)]),
+    dash.page_container
 ])
 
 if __name__ == '__main__':
