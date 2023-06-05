411ccc7389a6b786716702f47ab4498f8918a949
milan3000 <milanw12@gmail.com>
2023-06-05 18:40:08 +0200

    sub-pages

---
+++ pages/home.py
@@ -0,0 +1,16 @@
+import dash
+from dash import dcc
+from dash import html
+from plot_prediction import plot_prediction
+from plot_map import plot_map
+
+dash.register_page(__name__, path='/')
+
+fig1 = plot_prediction()
+fig2 = plot_map()
+
+# create the layout of the app
+layout = html.Div(children=[
+    html.Div([dcc.Graph(id='graph1',figure=fig1)]),
+    html.Div([dcc.Graph(id='graph2',figure=fig2)]),
+])
