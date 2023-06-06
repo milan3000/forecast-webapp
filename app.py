d63919f3034a6fc08dcc5dfa00c163f8169509d4
milan3000 <milanw12@gmail.com>
2023-06-06 15:17:55 +0200

    moved the data return from subpage to app route

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import dash
+import json
+import pandas as pd
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 from plot_prediction import plot_prediction
 from plot_map import plot_map
 
 app = dash.Dash(__name__, assets_folder='assets', use_pages=True)
 app.title = 'Renewable Energy Forecast Germany'
 server = app.server
+
+# Define an API endpoint that returns the JSON data
+@app.server.route('/api/data')
+def api_data():
+    df = pd.read_csv('current_prediction.csv')
+    json_data = df.to_dict(orient='records')
+    return json.dumps(json_data)
 
 app.layout = html.Div(children=[
     dash.page_container
